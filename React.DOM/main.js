f45c33db19df886d4776584581ade8a4456a5f34
Eduardo Santos Nascimento <eduardosantosnascimento@iMac-de-Eduardo.local>
2022-01-27 11:28:31 -0300

    ReactDOM feito

--- React.DOM/main.js
+++ React.DOM/main.js
@@ -1,22 +1,3 @@
-// "use strict";
-
-// function gerarLista() {
-
-//     const nav = React.creatElement("nav" ,{className: "nav"},"");
-//     ReactDOM.render(nav, document.getElementById("root"));
-
-//     const ul = React.creatElement("ul" ,{className: "ul"},"");
-//     ReactDOM.render(ul, document.getElementById("nav"));
-
-//     const li = React.creatElement("li" ,{className: "li"},"");
-//     ReactDOM.render(li, document.getElementById("ul"));
-
-//     const a = React.creatElement("a" ,{href: "index.html"},"Home");
-//     ReactDOM.render(a, document.getElementById("li"));
-
-// }
-
-// document.getElementById("gerarLista").addEventListener("click", gerarLista);
 function gerarLista() {
   function App() {
     return React.createElement(
